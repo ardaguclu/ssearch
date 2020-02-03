a58aa1a54bc617e84e815e98fc92a60034083f7f
Arda Güçlü <ardaguclu@gmail.com>
2020-02-03 23:56:01 +0300

    Add s3 object download into memory

--- internal/search/search.go
+++ internal/search/search.go
@@ -2,7 +2,12 @@ package search
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"runtime"
 	"sync"
+
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 
 	"github.com/aws/aws-sdk-go/aws/endpoints"
 
@@ -21,7 +26,8 @@ const (
 )
 
 type S struct {
-	sess *session.Session
+	sess       *session.Session
+	downloader *s3manager.Downloader
 }
 
 type SReq struct {
@@ -46,7 +52,8 @@ func NewS(env string) *S {
 	}
 
 	return &S{
-		sess: sess,
+		sess:       sess,
+		downloader: s3manager.NewDownloader(sess, func(d *s3manager.Downloader) { d.PartSize = S3DownloadPartSize }),
 	}
 }
 
@@ -73,7 +80,7 @@ func (s *S) Start(ctx context.Context, req *SReq) ([]s3.Object, error) {
 
 	err = c.ListObjectsV2PagesWithContext(ctx, loi,
 		func(page *s3.ListObjectsV2Output, lastPage bool) bool {
-			s := s.search(ctx, page.Contents, req.StartDate, req.EndDate)
+			s := s.search(ctx, page.Contents, *req)
 			if s != nil {
 				result = append(result, s...)
 			}
@@ -90,17 +97,17 @@ func (s *S) Start(ctx context.Context, req *SReq) ([]s3.Object, error) {
 
 // search executes search operation concurrently within the batch object files and
 // determines being found or not.
-func (s *S) search(ctx context.Context, contents []*s3.Object, sd, ed int64) []s3.Object {
+func (s *S) search(ctx context.Context, contents []*s3.Object, req SReq) []s3.Object {
 	var res []s3.Object
 
 	var wg sync.WaitGroup
 	var lock sync.Mutex
 
 	for _, c := range contents {
-		if sd != 0 && sd > c.LastModified.Unix() {
+		if req.StartDate != 0 && req.StartDate > c.LastModified.Unix() {
 			continue
 		}
-		if ed != 0 && ed < c.LastModified.Unix() {
+		if req.EndDate != 0 && req.EndDate < c.LastModified.Unix() {
 			continue
 		}
 
@@ -111,7 +118,7 @@ func (s *S) search(ctx context.Context, contents []*s3.Object, sd, ed int64) []s
 		wg.Add(1)
 		go func(ctx context.Context, obj s3.Object) {
 			defer wg.Done()
-			if s.found(ctx, *c.Key) {
+			if s.found(ctx, req.Bucket, *obj.Key, *obj.Size) {
 				lock.Lock()
 				defer lock.Unlock()
 
@@ -127,11 +134,26 @@ func (s *S) search(ctx context.Context, contents []*s3.Object, sd, ed int64) []s
 // found is used for core search algorithm being implemented,
 // it is designed for best performance. Since bucket in the S3 service expectedly stores huge amount of
 // files.
-func (s *S) found(ctx context.Context, key string) bool {
-	/*downloader := s3manager.NewDownloader(s.sess, func(d *s3manager.Downloader) {
-		d.PartSize = S3DownloadPartSize
-	})*/
+func (s *S) found(ctx context.Context, bucket string, key string, size int64) bool {
+	defer func() {
+		runtime.GC()
+	}()
 
+	content := aws.NewWriteAtBuffer(make([]byte, size))
+	_, err := s.downloader.DownloadWithContext(ctx,
+		content,
+		&s3.GetObjectInput{
+			Bucket: aws.String(bucket),
+			Key:    aws.String(key),
+		})
+
+	if err != nil {
+		log.Println("error occured ", err)
+		return false
+	}
+
+	result := content.Bytes()
+	fmt.Println(string(result))
 	// Knuth–Morris–Pratt (KMP) Algorithm
 	// Rabin-Karp Algorithm
 	return true
